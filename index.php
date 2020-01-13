556e10058e86adcc0160f4a27eb51c198232529f
Md. Shahinur Rahmangit <ridwanul.hafiz@gmail.com>
2020-01-13 09:32:16 +0600

    Some commit message

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once('Git.php');
 $repo = Git::open('D:\xampp\htdocs\gitTest');
 $repo->add('.');
 $repo->commit('Some commit message');
-$repo->push('origin', 'master');
+$repo->push('lang', 'master');
 print_r($repo);exit();
 
 
