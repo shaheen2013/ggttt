b63242d6e091ec3eacbd7989587fdaf4a55d5cb9
Md. Shahinur Rahmangit <ridwanul.hafiz@gmail.com>
2020-01-13 09:33:12 +0600

    Some commit message 13 Jan, 2020 04:33:12

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('Git.php');
 
 $repo = Git::open('D:\xampp\htdocs\gitTest');
 $repo->add('.');
-$repo->commit('Some commit message');
+$repo->commit('Some commit message '.date('d M, Y H:i:s'));
 $repo->push('lang', 'master');
 print_r($repo);exit();
 
